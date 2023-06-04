1af97f69f186afd509ed89b0dc66ddc3d2327586
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-06-04 10:48:59 +0300

    feat: implements Repository

---
+++ src/main/java/com/example/demo/repository/Repository.java
@@ -0,0 +1,45 @@
+package com.example.demo.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@org.springframework.stereotype.Repository
+public class Repository {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private String request = read("SearchProduct.sql");
+
+
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<String> getProductName(String name) {
+        Map<String, String> param = new HashMap<>();
+        param.put("name", name);
+        List<String> product_name = namedParameterJdbcTemplate.queryForList(request, param, String.class);
+        return product_name;
+    }
+}
